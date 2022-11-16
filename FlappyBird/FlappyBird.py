76257abb0523f5dbb61a130bbeac1b4ef809e11c
Gabriel Costa Santos <104160744+GabiCostt@users.noreply.github.com>
2022-11-16 18:51:27 -0300

    novas def

--- FlappyBird/FlappyBird.py
+++ FlappyBird/FlappyBird.py
@@ -22,3 +22,115 @@ IMAGENS_PASSARO = [
 #Fonte do Texto de pontuação do game
 pygame.font.init()  #Iniciar a fonte
 FONTE_PONTOS = pygame.font.SysFont('arial', 50) #Nome e tamanho da fonte respectivamente
+
+class Passaro:
+    IMGS = IMAGENS_PASSARO
+    #Animações das rotações
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0
+        self.imagem = self.IMGS[0]
+
+    def pular(self):
+        self_velocidade = -10.5
+        self.tempo = 0
+        self.altura  = self.y
+
+    def mover(self):
+        #calcular o deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+
+        #Restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        #Angulo do passaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            self.angulo = self.ROTACAO_MAXIMA
+        elif self.angulo > -90:
+            self.angulo -= self.VELOCIDADE_ROTACAO 
+
+    def desenhar(self, tela):
+        # definir a imagem do passaro que vai ser usada
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+            self.imagem = self.IMGS[0]
+            self.contagem_imagem = 0
+        
+        # se o passaro estiver caindo não vai bater asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO*2
+
+        # desenhar a imagem
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        pos_centro_imagem = self.imagem.get_rect(topletf=(self.x, self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
+
+class Cano:
+    DISTANCIA = 200
+    VELOCIDADE = 5
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.pos_topo = 0
+        self.pos_base = 0
+        self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO  
+        self.passou = False
+        self.definir_altura()   
+
+    def definir_altura(self):
+        self.altura =  random.randrange(50, 450)
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height()
+        self.pos_base = self.altura + self.DISTANCIA
+
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x, self.pos_topo))
+        tela.blit(self.CANO_BASE, (self.x, self.pos_base))
+
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x, self.pos_topo - round(passaro.y))
+        distancia_base = (self.x - passaro.x, self.pos_base - round(passaro.y))
+
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)        
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if topo_ponto or base_ponto:
+            return True
+        else:
+            return False
